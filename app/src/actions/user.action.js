7d2b786d1f7e1fbcced060403c34296b23616c66
Isaac <theisaacmj@gmail.com>
2022-02-26 11:48:12 -0800

    Initial redux setup

---
+++ app/src/actions/user.action.js
@@ -0,0 +1,7 @@
+// actions
+export const GET = 'user/GET';
+export const GET_ALL = 'user/GET_ALL';
+export const UPDATE = 'user/UPDATE';
+export const DELETE = 'user/DELETE';
+
+// action creators
---
+++ app/src/reducers/userReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  userList: [],
+  selectedUser: null
+};
+
+const userReducer = (state = initialState, action = {}) => {
+  switch (action.type) {
+    default:
+      return initialState;
+  }
+};
+
+export default userReducer;
